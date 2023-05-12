def6aed726e1d6ac8539eb7ff54308fb77350f7a
Nils Schneider <nils.schneider@posteo.de>
2023-05-12 10:22:50 +0200

    Monoid and colors

--- Device.hs
+++ Device.hs
@@ -16,10 +16,13 @@ import Control.Monad
 import Data.Binary.Get
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Time.Clock
+import System.Console.Pretty
 
 data DeviceState = DeviceState 
     { stateMap :: Map GroupAddress IncomingGroupMessage
     , waitingAddresses :: Map GroupAddress (Get DPT)
+    , scheduledActions :: [(UTCTime, Device DeviceState ())]
     }
 
 instance Show DeviceState where
@@ -27,12 +30,30 @@ instance Show DeviceState where
         where
             showKeys = map (\(k, _) -> k) . Map.toList
 
-newtype Device s a = Device { runDevice :: DeviceState -> (a, DeviceState, [GroupMessage]) }
+instance Semigroup DeviceState where
+    state1 <> state2 = mergeDeviceStates state1 state2
+
+instance Monoid DeviceState where
+    mempty = initialDeviceState
+
+initialDeviceState :: DeviceState
+initialDeviceState = DeviceState Map.empty Map.empty []
+
+mergeDeviceStates :: DeviceState -> DeviceState -> DeviceState
+mergeDeviceStates state1 state2 = DeviceState
+    { stateMap = Map.union (stateMap state1) (stateMap state2)
+    , waitingAddresses = Map.union (waitingAddresses state1) (waitingAddresses state2)
+    , scheduledActions = scheduledActions state1 ++ scheduledActions state2
+    }
+
+data Device s a 
+    = Device { runDevice :: DeviceState -> (a, DeviceState, [GroupMessage]) }
+    | Delay { runDelay :: NominalDiffTime -> Device s a }
 
 instance Functor (Device s) where
     fmap f device = Device $ \s -> 
         let (a, s', msgs) = runDevice device s
-        in (f a, s', msgs)
+        in (f a, s' `mappend` s, msgs)
 
 instance Applicative (Device s) where
     pure a = Device $ \s -> (a, s, [])
@@ -40,19 +61,13 @@ instance Applicative (Device s) where
     deviceF <*> deviceA = Device $ \s ->
         let (f, s'@DeviceState{waitingAddresses=waitF, stateMap=stateF}, msgsF) = runDevice deviceF s
             (a, s''@DeviceState{waitingAddresses=waitA, stateMap=stateA}, msgsA) = runDevice deviceA s'
-        in (f a, s''{waitingAddresses = Map.union waitF waitA, stateMap = Map.union stateF $ Map.union stateA $ stateMap s}, msgsF ++ msgsA)
+        in (f a, s'' `mappend` s', msgsF ++ msgsA)
 
 instance Monad (Device s) where
     device >>= f = Device $ \s ->
         let (a, s'@DeviceState{waitingAddresses=wait1, stateMap=state1}, msgs) = runDevice device s
             (b, s''@DeviceState{waitingAddresses=wait2, stateMap=state2}, msgs') = runDevice (f a) s'
-        in (b, s''{waitingAddresses = Map.union wait1 wait2, stateMap = Map.union state1 $ Map.union state2 $ stateMap s}, msgs ++ msgs')
-
-initialDeviceState :: DeviceState
-initialDeviceState = DeviceState Map.empty Map.empty
-
-getState :: Device s DeviceState
-getState = Device $ \s -> (s, s, [])
+        in (b, s'' `mappend` s', msgs ++ msgs')
 
 modifyState :: (DeviceState -> DeviceState) -> Device s ()
 modifyState f = Device $ \s -> ((), f s, [])
@@ -71,20 +86,20 @@ processDeviceState knx msg (device, state) = do
         then do
             let updatedState = newState { stateMap = Map.insert (incomingGroupAddress msg) msg (stateMap newState) }
 
-            putStrLn $ "Processing device state..."            
+            putStrLn $ color Green $ "Processing device state..."       
 
-            putStrLn $ "    Received message: " ++ show msg
-            putStrLn $ "    Current state: " ++ show updatedState
+            putStrLn $ color Green $ "    Received message: " ++ show msg
+            putStrLn $ color Green $ "    Current state: " ++ show updatedState
 
             let (a, finalState, outputMessages) = runDevice device updatedState
 
-            putStrLn $ "    Final state: " ++ show finalState
+            putStrLn $ color Blue $ "    Final state: " ++ show finalState
 
             unless (null outputMessages) $ do
-                putStrLn $ "    Performing device actions..."
+                putStrLn $ color Green $ "    Performing device actions..."
 
                 performDeviceActions knx outputMessages
-                putStrLn $ "    Device actions completed."
+                putStrLn $ color Green $  "    Device actions completed."
 
             return finalState
         else
@@ -93,7 +108,7 @@ processDeviceState knx msg (device, state) = do
 performDeviceActions :: KNXConnection -> [GroupMessage] -> IO ()
 performDeviceActions knx outputMessages = do
     mapM_ (\outputMessage -> do
-          putStrLn $ "Output message: " ++ show outputMessage
+          putStrLn $ color Red $ "Output message: " ++ show outputMessage
           -- Send the output messages to the KNX bus
           KNX.groupWrite knx outputMessage
           ) outputMessages
@@ -102,7 +117,7 @@ waitFor :: GroupAddress -> (Get DPT) -> Device DeviceState (Maybe DPT)
 waitFor addr parser = Device $ \s ->
     let stateMap' = stateMap s
     in case Map.lookup addr stateMap' of
-        Just msg -> (parseMsg msg, s { stateMap = Map.delete addr stateMap' }, [])
+        Just msg -> (parseMsg msg, s, [])
         Nothing -> (Nothing, s { waitingAddresses = Map.insert addr parser (waitingAddresses s) }, [])
     where
         parseMsg :: IncomingGroupMessage -> Maybe DPT
@@ -119,9 +134,19 @@ sampleDevice = do
 
     case (a, b) of
         (Just (DPT18_1 (False, val_a)), Just (DPT18_1 (False, val_b))) -> do
-            c <- waitFor (GroupAddress 0 1 20) parseDPT18_1
+            c <- waitFor (GroupAddress 0 1 (20 + val_a + val_b)) parseDPT18_1
             case c of
                 Just (DPT18_1 (False, val_c)) -> do
                     groupWrite (GroupAddress 0 1 21) (DPT18_1 (False, val_a + val_b + val_c))
                 _ -> return ()
+        _ -> return ()
+
+-- Scene multiplexer
+sceneMultiplexer :: GroupAddress -> Int -> GroupAddress -> Device DeviceState ()
+sceneMultiplexer inputAddr offset outputAddr = do
+    a <- waitFor inputAddr parseDPT18_1
+
+    case a of
+        Just (DPT18_1 (False, val_a)) -> do
+            groupWrite outputAddr (DPT18_1 (False, (val_a `mod` 4) + offset))
         _ -> return ()
