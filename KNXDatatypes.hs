1c7ec4bd6905271e1f8046b78a90cb26a014cc42
Nils Schneider <nils.schneider@posteo.de>
2023-05-18 19:35:43 +0200

    KNXFloat16: remove debug traces

--- KNXDatatypes.hs
+++ KNXDatatypes.hs
@@ -16,8 +16,6 @@ import Data.Time.Calendar
 
 import GHC.Float
 
-import Debug.Trace
-
 data KNXTimeOfDay = KNXTimeOfDay
     { knxWeekDay :: Maybe DayOfWeek
     , knxTimeOfDay :: TimeOfDay
@@ -61,36 +59,19 @@ putKNXFloat16 v =
             mantissaWord = fromIntegral $ max (-2048) (min 2046 mantissaInt) :: Word16
             mantissaBits = mantissaWord .&. 0x87FF
             exponentBits = fromIntegral $ (e' `shiftL` 11) .&. 0x7800
-        in  trace ("v:  " ++ show (word64ToBits $ castDoubleToWord64 v))
-            trace ("v': " ++ show (word64ToBits $ castDoubleToWord64 v'))
-            trace ("m': " ++ show (word64ToBits $ castDoubleToWord64 m'))
-            trace ("w:  " ++ show (word16ToBits $ fromIntegral mantissaInt))
-            trace ("e': " ++ show e')
-            trace ("putKNXFloat16: " ++ show v ++ " -> m: "
-                    ++ word16ToBits mantissaBits ++ ", e: "
-                    ++ word16ToBits exponentBits) $
-            putWord16be . fromIntegral $ exponentBits .|. mantissaBits
+        in putWord16be . fromIntegral $ exponentBits .|. mantissaBits
         where
             scaleExponent :: Int -> Int -> Int -> (Double, Int) -> (Double, Int)
             scaleExponent offset low high (m, e) = 
                 let s' = e - e' + offset
                     m' = scaleFloat (s' - offset) m
-                in trace ("scaleExponent: " ++ show (m, e) ++ " -> " ++ show (m', e'))
-                    (m', e')
+                in (m', e')
                 where
                     e' = case () of
                         _ | e + offset < low -> low
                           | e + offset > high -> high
                           | otherwise -> e + offset
                           
-            word16ToBits :: Word16 -> String
-            word16ToBits w = let s = concatMap (\i -> if w .&. (1 `shiftL` i) /= 0 then "1" else "0") [15,14..0]
-                            in take 1 s ++ " " ++ take 4 (drop 1 s) ++ " " ++ take 11 (drop 5 s)
-
-            word64ToBits :: Word64 -> String
-            word64ToBits w = let s = concatMap (\i -> if w .&. (1 `shiftL` i) /= 0 then "1" else "0") [63,62..0]
-                            in take 1 s ++ " " ++ take 8 (drop 1 s) ++ " " ++ take 52 (drop 9 s)
-
 getKNXFloat16 :: Get Double
 getKNXFloat16 = do
     mantissaExponent <- getWord16be
