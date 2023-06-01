596a89b22e7fddf7e4b4c939fd49eb524708b6cc
Nils Schneider <nils.schneider@posteo.de>
2023-06-02 01:28:52 +0200

    TimeSender: respond to group reads

--- TimeSender.hs
+++ TimeSender.hs
@@ -25,18 +25,36 @@ timeSender :: TimeSenderConfig -> Device
 timeSender conf = makeDevice "TimeSender" () (timeSenderF conf)
 
 timeSenderF :: TimeSenderConfig -> DeviceM () ()
-timeSenderF conf = loop
+timeSenderF conf = do
+  respondOnRead (timeGA conf) $ do
+    debug "Time read"
+    time <- zonedTimeToLocalTime <$> getTime
+    Just <$> getTimeDpt time
+
+  respondOnRead (dateGA conf) $ do
+    debug "Date read"
+    time <- zonedTimeToLocalTime <$> getTime
+    Just <$> getDateDpt time
+
+  loop
+
   where
     loop = do
       debug "Sending time"
       time <- zonedTimeToLocalTime <$> getTime
-      let day = localDay time
-      let dptTime = DPT10 $ KNXTimeOfDay (Just $ dayOfWeek day) (localTimeOfDay time)
-      groupWrite (timeGA conf) dptTime
-
-      let dptDate = DPT11 day
-      groupWrite (dateGA conf) dptDate
+      getTimeDpt time >>= groupWrite (timeGA conf)
+      getDateDpt time >>= groupWrite (dateGA conf)
 
       scheduleIn (intervalSeconds conf) loop
 
       return ()
+
+    getTimeDpt time = do
+      let day = localDay time
+      let dptTime = DPT10 $ KNXTimeOfDay (Just $ dayOfWeek day) (localTimeOfDay time)
+      return dptTime
+
+    getDateDpt time = do
+      let day = localDay time
+      let dptDate = DPT11 day
+      return dptDate
